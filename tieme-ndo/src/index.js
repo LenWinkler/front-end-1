c7a4afcb30ed8cbdb613dfc4363c4a74955b5ec6
LenWinkler <lenwinkler86@gmail.com>
2019-10-21 14:00:29 -0400

    set up store and basic file structure

--- tieme-ndo/src/index.js
+++ tieme-ndo/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleWare } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import Reducer from './reducer/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(Reducer, applyMiddleWare(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById('root'));
