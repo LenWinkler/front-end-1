51b4d1967732997f8aa3c6e8014ae1ece99b44da
LenWinkler <lenwinkler86@gmail.com>
2019-10-21 17:04:14 -0400

    filled out sign in and sign up forms

--- tieme-ndo/src/components/Forms/SignUp.js
+++ tieme-ndo/src/components/Forms/SignUp.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import axiosWithAuth from '../Auth/AxiosWithAuth';
+import styled from 'styled-components';
+
+const PassMatch = styled.p`
+    color: green;
+`
+
+const NoPassMatch = styled.p`
+    color: red;
+`
 
 const SignUp = () => {
     const [user, setUser] = useState('');
     const [pass, setPass] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
     const [email, setEmail] = useState('');
 
     const userHandler = e => {
@@ -11,8 +21,38 @@ const SignUp = () => {
     };
 
     const passHandler = e => {
-        
+        setPass(e.target.value)
+    };
+
+    const confirmHandler = e => {
+        setConfirmPass(e.target.value)
+    };
+
+    const emailHandler = e => {
+        setEmail(e.target.value)
+    };
+
+    const register = e => {
+        e.preventDefault();
+
+        axiosWithAuth()
+        .post('/api/register')
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
     }
+
+    return (
+        <div>
+            <form onSubmit={register}>
+                <input type="text" value={user} onChange={userHandler} placeholder="Username" />
+                <input type="password" value={pass} onChange={passHandler} placeholder="Password" />
+                <input type="password" value={confirmPass} onChange={confirmHandler} placeholder="Confirm Password" />
+                {pass === confirmPass ? <PassMatch>Passwords match!</PassMatch> : <NoPassMatch>Passwords must match!</NoPassMatch>}
+                <input type="email" value={email} onChange={emailHandler} placeholder="Email Address" />
+                <button>Register</button>
+            </form>
+        </div>
+    )
 
 }
 
