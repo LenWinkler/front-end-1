4876c401e79f3863c2d82aaa4510114e82a06b65
LenWinkler <lenwinkler86@gmail.com>
2019-10-21 14:59:31 -0400

    added react-router-dom, built basic reducer, built private route component

--- tieme-ndo/src/index.js
+++ tieme-ndo/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { createStore, applyMiddleWare } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import Reducer from './reducer/index';
+import reducer from './reducer/index';
 
-const store = createStore(Reducer, applyMiddleWare(thunk));
+const store = createStore(reducer, applyMiddleWare(thunk));
 
 ReactDOM.render(
 <Provider store={store}>
---
+++ tieme-ndo/src/reducer/index.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+    farmers: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
