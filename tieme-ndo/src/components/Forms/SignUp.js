31e3d2191de92e78c203c4f5502d0c7af7d86e96
LenWinkler <lenwinkler86@gmail.com>
2019-10-24 15:02:29 -0400

    styled login and sign up pages

--- tieme-ndo/src/components/Forms/SignUp.js
+++ tieme-ndo/src/components/Forms/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axiosWithAuth from '../Auth/AxiosWithAuth';
 import styled from 'styled-components';
+import Header from '../Misc/Header'
 
 const PassMatch = styled.p`
     color: green;
@@ -8,6 +9,45 @@ const PassMatch = styled.p`
 
 const NoPassMatch = styled.p`
     color: red;
+`
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+`
+
+const UserInput = styled.input`
+    height: 2rem;
+    width: 16rem;
+    margin-top: 4rem;
+    margin-bottom: 1rem;
+    font-size: 1rem;
+    border: 1px solid #4e4e4e;
+`
+
+const OtherInput = styled.input`
+    height: 2rem;
+    width: 16rem;
+    margin: 1rem 0;
+    font-size: 1rem;
+    border: 1px solid #4e4e4e;
+`
+
+const Button = styled.button`
+    width: 12rem;
+    height: 3rem;
+    margin: 2rem 0;
+    border-radius: 5px;
+    background: #73A85A;
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+
+    :hover {
+        background: white;
+        color: #73A85A;
+        border: 1px solid #73A85A;
+    }
 `
 
 const SignUp = props => {
@@ -71,17 +111,20 @@ const SignUp = props => {
     }
 
     return (
+        <>
+        <Header title={`Register New User`} />
         <div>
-            <form onSubmit={register}>
-                <input type="text" name="user" value={user} onChange={userHandler} placeholder="Username" />
-                <input type="password" name="password" value={pass} onChange={passHandler} placeholder="Password" />
+            <StyledForm onSubmit={register}>
+                <UserInput type="text" name="user" value={user} onChange={userHandler} placeholder="Username" />
+                <OtherInput type="password" name="password" value={pass} onChange={passHandler} placeholder="Password" />
                 {passLength()}
-                <input type="password" name="confirm password" value={confirmPass} onChange={confirmHandler} placeholder="Confirm Password" />
+                <OtherInput type="password" name="confirm password" value={confirmPass} onChange={confirmHandler} placeholder="Confirm Password" />
                 {passConfirm()}
-                <input type="email" name="email" value={email} onChange={emailHandler} placeholder="Email Address" />
-                <button>Register</button>
-            </form>
+                <OtherInput type="email" name="email" value={email} onChange={emailHandler} placeholder="Email Address" />
+                <Button>Register</Button>
+            </StyledForm>
         </div>
+        </>
     )
 
 }
